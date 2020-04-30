061b51052494a3dd2b6e33932ad3c1d810abf639
sezeryildirim <sezer.yildirim@live.com>
2020-04-30 03:32:59 +0300

    Added MongoDB Driver and Entities

---
+++ WarrantyTracking.Entities/Detail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarrantyTracking.Entities
+{
+    public class Detail
+    {
+        public String serial_number { get; set; }
+        public Double price { get; set; }
+        public String battery_type { get; set; }
+        public String ampere { get; set; }
+        public String brand { get; set; }
+        public String description { get; set; }
+        public DateTime date { get; set; }
+        public Boolean is_active { get; set; }
+        public String car_brand { get; set; }
+        public String car_model { get; set; }
+        public String customer_name { get; set; }
+        public String[] battery_property { get; set; }
+    }
+}
---
+++ WarrantyTracking.Entities/Warranty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+
+namespace WarrantyTracking.Entities
+{
+    public class Warranty
+    {
+        public ObjectId _id { get; private set; }
+        public String license_plate { get; set; }
+        public List<Detail> Details { get; set; }
+        public DateTime created_date { get; set; }
+        public DateTime updated_date { get; set; }
+    }
+}
